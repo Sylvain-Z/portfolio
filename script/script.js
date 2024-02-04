286e62d0f852c2ceded6eb15c30dfb80a39013ce
Sylvain-Z <sylvain.zoogones@gmail.com>
2024-02-04 23:22:40 +0100

    V1

---
+++ script/script.js
@@ -0,0 +1,81 @@
+'use strict';
+
+/***********************************************************************************/
+/* *********************************** DONNEES *************************************/
+/***********************************************************************************/
+
+
+/*  Projets  */
+const toggleShow = document.getElementsByClassName('toggleshow');
+const toggleHide = document.getElementsByClassName('toggleHide');
+const content = document.getElementsByClassName('contentToToggle');
+
+/* Liste des compétences */
+
+const skillsIllustration = document.getElementsByClassName('recto');
+const skillsList = document.getElementsByClassName('verso');
+const skillsShow = document.getElementsByClassName('skillsShow');
+const skillsHide = document.getElementsByClassName('skillsHide');
+
+
+
+/***********************************************************************************/
+/* ********************************** FONCTIONS ************************************/
+/***********************************************************************************/
+
+
+/* TOGGLE CHEVRONS PROJECTS */
+
+function showContent() {
+    for (let i = 0; i < toggleShow.length; i++) {
+        toggleShow[i].addEventListener('click', () => {
+            content[i].classList.remove('toggle');
+            toggleHide[i].classList.add('fa-solid', 'fa-chevron-up', 'fa-xl');
+            toggleShow[i].classList.remove('fa-solid', 'fa-chevron-down', 'fa-xl');
+        });
+    }
+}
+function hideContent() {
+    for (let i = 0; i < toggleHide.length; i++) {
+        toggleHide[i].addEventListener('click', () => {
+            content[i].classList.add('toggle');
+            toggleHide[i].classList.remove('fa-solid', 'fa-chevron-up', 'fa-xl');
+            toggleShow[i].classList.add('fa-solid', 'fa-chevron-down', 'fa-xl');
+        });
+    }
+}
+
+/* TOGGLE SKILLS LIST */
+
+function showList() {
+    for (let i = 0; i < skillsIllustration.length; i++) {
+        skillsIllustration[i].addEventListener('click', () => {
+            skillsList[i].classList.remove('hidenList');
+            skillsShow[i].classList.remove('fa-solid', 'fa-chevron-down', 'fa-xl');
+            skillsHide[i].classList.add('fa-solid', 'fa-chevron-up', 'fa-xl');
+        });
+    }
+}
+function hideList() {
+    for (let i = 0; i < skillsIllustration.length; i++) {
+        skillsList[i].addEventListener('click', () => {
+            skillsList[i].classList.add('hidenList');
+            skillsShow[i].classList.add('fa-solid', 'fa-chevron-down', 'fa-xl');
+            skillsHide[i].classList.remove('fa-solid', 'fa-chevron-up', 'fa-xl');
+        });
+    }
+}
+
+
+/************************************************************************************/
+/* ******************************** CODE PRINCIPAL **********************************/
+/************************************************************************************/
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    showContent();
+    hideContent();
+
+    showList();
+    hideList();
+});
